e52f00ad04e8467951a5d3e6cf6f60b04f0d2061
Marius <47770722+marixm@users.noreply.github.com>
2019-03-04 09:35:38 +0200

    Add files via upload

---
+++ C#/ContosoUniversity/Models/Iranga.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Iranga
+    {
+        [Key]
+        [ForeignKey("Student")]
+        public int StudentID { get; set; }
+        [StringLength(50)]
+        [Display(Name = "Room Facilities")]
+        public string Location { get; set; }
+
+        public virtual Student Student { get; set; }
+    }
+}
---
+++ C#/ContosoUniversity/Models/Kambays.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
+{
+    public class Kambarys
+    {
+        [Key]
+        [ForeignKey("Student")]
+        public int StudentID { get; set; }
+        [StringLength(50)]
+        [Display(Name = "Student Room")]
+        public string Location { get; set; }
+
+        public virtual Student Student { get; set; }
+    }
+}
